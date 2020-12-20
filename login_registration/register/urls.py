05af32be0d6063369d950d02edd79a43d06989c6
Devika Shailesh Olkar <devikaolkar@kccemsr.edu.in>
2020-12-20 21:06:28 +0530

    Added styling

--- login_registration/register/urls.py
+++ login_registration/register/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views
 
+appname='accounts'
+
 urlpatterns = [
 path("register/",views.register,name="register"),
-path("logout/", views.logout, name="logout")
+path("logout/", views.logoutPage, name="logoutPage"),
+path('login/',views.loginPage,name='loginPage'),
 ]
--- login_registration/register/views.py
+++ login_registration/register/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from .forms import RegisterForm
+from django.contrib.auth import authenticate,login,logout
 # Create your views here.
 
 def register(response):
@@ -15,5 +16,16 @@ def register(response):
     }
     return render(response,"register/register.html",context)
 
-def logout(request):
+def logoutPage(request):
     return render(request,"registration/logout.html")
+
+def loginPage(request):
+    if request.method=='POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect("/")
+    return render(request,"registration/login.html")
